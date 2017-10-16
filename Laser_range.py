8b73c751f9b4cf7bf04db4c861bab401f8b0fcb5
oskarboer <oskarboer@users.noreply.github.com>
2017-10-16 14:44:04 +0300

    Add files via upload

--- Laser_range.py
+++ Laser_range.py
@@ -3,8 +3,9 @@
 
 
 import cv2
-from time import *
+import time
 import numpy
+
 
 # Callback Function for Trackbar (but do not any work)
 def nothing(*arg):
@@ -12,93 +13,81 @@ def nothing(*arg):
 
 
 cv2.namedWindow("capture")
-cv2.createTrackbar("Treshold", "capture", 100, 255, nothing)
+cv2.createTrackbar("Threshold", "capture", 100, 255, nothing)
 
 capture = cv2.VideoCapture(0)
 
-width = 640
-height = 480
-k_senseble = 0
 score = 0
 font = cv2.FONT_HERSHEY_SIMPLEX
 
 
-
 def make_mask(dframe):
-	targets = []
-	width, height = 640, 480
+    targets = []
+    height, width = dframe.shape[:2]
 
-	for y in range(0, height):
-		for x in range(0, width):
-			if dframe[y][x] == 255: #check if zone filled
-				mask = numpy.zeros((height + 2, width + 2), numpy.uint8) #создаем массив с будущей зоной\\create zone list
-				cv2.floodFill(dframe, mask, (x,y), 10) #fill the zone and repaint it in gray(10)
-				targets.append((mask[0:height, 0:width], sum(sum(mask)))) #save the size of zone and mask
-	
-	
-	print("ready")
-	return targets
+    for y in range(0, height):
+        for x in range(0, width):
+            if dframe[y][x] == 255:  # check if zone filled
+                mask = numpy.zeros((height + 2, width + 2),
+                                   numpy.uint8)  # создаем массив с будущей зоной\\create zone list
+                cv2.floodFill(dframe, mask, (x, y), 10)  # fill the zone and repaint it in gray(10)
+                targets.append((mask[1:(height + 1), 1:(width + 1)], sum(sum(mask))))  # save the size of zone and mask
 
-
+    print("ready")
+    return targets
 
 
-
-
-
-	
-while cv2.waitKey(1) != 32: #spacebar
-	k_tresh = cv2.getTrackbarPos("Treshold", "capture")
-	cframe = capture.read()[1]
-	gframe = cv2.cvtColor(cframe, cv2.COLOR_RGB2GRAY)
-	value, tframe = cv2.threshold(gframe, k_tresh, 255, cv2.THRESH_BINARY)
-	cv2.imshow("capture", tframe)
-
-
-		
-
+while cv2.waitKey(1) != 32:  # space bar
+    k_tresh = cv2.getTrackbarPos("Threshold", "capture")
+    cframe = capture.read()[1]
+    gframe = cv2.cvtColor(cframe, cv2.COLOR_RGB2GRAY)
+    reval, tframe = cv2.threshold(gframe, k_tresh, 255, cv2.THRESH_BINARY)
+    cv2.imshow("capture", tframe)
 
 targets = make_mask(tframe)
-
-
-
 
 pframe = capture.read()[1]
 pframe = cv2.cvtColor(pframe, cv2.COLOR_RGB2GRAY)
 
+height, width = pframe.shape[:2]
+last_hit_target = 0
+last_hit_time = 0
+
 while True:
-	
-	cframe = capture.read()[1]
-	gframe = cv2.cvtColor(cframe, cv2.COLOR_RGB2GRAY)
-	dframe = cv2.absdiff(gframe,pframe)
-	value, tframe = cv2.threshold(dframe, 70, 255, cv2.THRESH_BINARY)
 
-	
+    cframe = capture.read()[1]
+    gframe = cv2.cvtColor(cframe, cv2.COLOR_RGB2GRAY)
+    dframe = cv2.absdiff(gframe, pframe)
+    retval, tframe = cv2.threshold(dframe, 70, 255, cv2.THRESH_BINARY)
 
-	d = []
-	y = 0
+    results = []
+    y = 0
 
-	for i in range(1, len(targets)):
-		d.append(sum(sum(tframe&targets[i][0])))
+    for i in range(0, len(targets)):
+        results.append(sum(sum(tframe & targets[i][0])))
 
+    max_number_of_pixels = max(results)
 
-	
-	c = max(d)
-	s = d.index(c)
-	if c > k_senseble:
-		print(s)
-		score += 1
-		sleep(0.1)
-		
-	
-		
-	cv2.putText(tframe, "score %i"%score, (4,460), font, 1, [255])
-	cv2.imshow("capture", tframe)
-	pframe = gframe
+    shot_target_index = results.index(max_number_of_pixels)
 
-	if cv2.waitKey(1) == 27:
-		break
+    # check if there more then 10 pixels
+    if ((max_number_of_pixels > 250) and ((abs(last_hit_time - time.time()) > 0.25) or (last_hit_time == 0))):
+        print(shot_target_index)
+        score += shot_target_index
+        last_hit_time = time.time()
 
-cv2.ReleaseCapture(capture)
-cv2.DestroyWindow("capture")
+        # remember last successful hit
+        if shot_target_index:
+            last_hit_target = shot_target_index
 
+    cv2.putText(tframe, "score %i" % score, (10, height - 20), font, 1, [255])
+    cv2.putText(tframe, "Hit %i" % last_hit_target, (int(width / 2), height - 20), font, 1, [255])
 
+    cv2.imshow("capture", tframe)
+    pframe = gframe
+
+    if cv2.waitKey(1) == 27:  # ESC
+        break
+
+capture.release()
+cv2.destroyAllWindows()
